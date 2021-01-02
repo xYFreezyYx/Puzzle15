b8f37fc4cef5e28060fb016640ef7a20f0397fb1
xYFreezyYx <72568023+xYFreezyYx@users.noreply.github.com>
2021-01-02 13:10:16 +0200

    02.01.2021 Upadate

--- Puzzle15/Form1.cs
+++ Puzzle15/Form1.cs
@@ -12,9 +12,73 @@ namespace Puzzle15
 {
     public partial class Puzzle : Form
     {
+        List<Button> tiles = new List<Button>();
+        Random rand = new Random();
+
         public Puzzle()
         {
             InitializeComponent();
+            InitializPuzzle();
+            ShuffleTiles();
+        }
+
+        private void InitializPuzzle()
+        {
+            int tileCounter = 1;
+            Button tile = null;
+            for (int j = 0; j < 4; j++)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    tile = new Button();                   
+                    tile.BackColor = Color.SteelBlue;
+                    tile.Width = 80;
+                    tile.Height = 80;
+                    tile.Top = 20 + j * 90;
+                    tile.Left = 20 + i * 90;
+                    tile.Text = tileCounter.ToString();
+
+                    if (tileCounter == 16)
+                    {
+                        tile.Text = string.Empty;
+                        tile.Name = "TileEmpty";
+                    }
+                    else
+                    {
+                        tile.Text = tileCounter.ToString();
+                    }
+
+                    this.Controls.Add(tile);
+                    tiles.Add(tile);
+
+                    tile.Click += Tile_Click;
+
+                    tileCounter++;
+                }
+            }
+        }
+
+        private void Tile_Click(object sender, EventArgs e)
+        {
+            Button tile = (Button)sender;
+            SwapTiles(tile);
+        }
+
+        private void SwapTiles(Button tile)
+        {
+            Button tileEmpty = (Button)this.Controls["TileEmpty"];
+
+            Point tileOldLocatione = tile.Location;
+            tile.Location = tileEmpty.Location;
+            tileEmpty.Location = tileOldLocatione;
+        }
+
+        private void ShuffleTiles()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                SwapTiles(tiles[rand.Next(0, 15)]);
+            }
         }
     }
 }
